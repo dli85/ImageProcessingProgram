1c30cccc47f69fda9c22e629a69884620a19ead5
Edwin Li <edwinliiiii@Edwins-MacBook-Pro.local>
2022-06-08 10:56:05 -0400

    added vertical flip test

--- test/ImageProcessingControllerImplTest.java
+++ test/ImageProcessingControllerImplTest.java
@@ -25,28 +25,24 @@ public class ImageProcessingControllerImplTest {
   //Checks that the pixel representation for two images are exactly the same.
   private void testTwoImagesAreTheSame(SimpleImageProcessingModel model,
                                        String image1Name, String image2Name) {
-      //First we check that the width and height of the images are the same
-      int width = model.getWidth(image1Name);
-      int height = model.getHeight(image1Name);
+    //First we check that the width and height of the images are the same
+    int width = model.getWidth(image1Name);
+    int height = model.getHeight(image1Name);
 
-      assertEquals(0, width - model.getWidth(image2Name));
-      assertEquals(0, height - model.getHeight(image2Name));
+    assertEquals(0, width - model.getWidth(image2Name));
+    assertEquals(0, height - model.getHeight(image2Name));
 
-      for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-          Map<PixelProperty, Integer> img1Values = model.getPixelInfo(image1Name, i, j);
-          Map<PixelProperty, Integer> img2Values = model.getPixelInfo(image1Name, i, j);
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        Map<PixelProperty, Integer> img1Values = model.getPixelInfo(image1Name, i, j);
+        Map<PixelProperty, Integer> img2Values = model.getPixelInfo(image1Name, i, j);
 
-          //We check that every pixel value is the same.
-          for(PixelProperty p : img1Values.keySet()) {
-            assertEquals(img1Values.get(p), img2Values.get(p));
-          }
+        //We check that every pixel value is the same.
+        for (PixelProperty p : img1Values.keySet()) {
+          assertEquals(img1Values.get(p), img2Values.get(p));
         }
       }
-
-
-
-
+    }
   }
 
   /**
@@ -96,5 +92,20 @@ public class ImageProcessingControllerImplTest {
                     "load test/images/mudkip.ppm mudkip2 q"));
     controller.start();
     testTwoImagesAreTheSame(model1, "mudkip1", "mudkip2");
+  }
+
+  @Test
+  public void testVerticalFlip() {
+    SimpleImageProcessingModel model1 = new SimpleImageProcessingModel();
+
+    ImageProcessingController controller = new ImageProcessingControllerImpl(
+            model1, new ImageProcessingViewImpl(model1, new StringBuilder()),
+            new StringReader("load test/images/mudkip.ppm mudkip1 \n" +
+                    "load test/images/testMudkip.ppm mudkip2 \n" +
+                    "load test/images/gimp-vertical-mudkip.ppm verticalMudkip1 \n" +
+                    "vertical-flip mudkip1 verticalMudkip2 q"));
+    controller.start();
+
+    testTwoImagesAreTheSame(model1, "verticalMudkip1", "mudkip2");
   }
 }
