65f472295f54cbeb236c1615da1da975c65a6635
David Li <dali7758769@gmail.com>
2022-06-24 14:12:26 -0400

    Update jar name, update readme

--- README.md
+++ README.md
@@ -9,10 +9,10 @@ See the usage section for a GUI usage guide.
 This is an application to process images. It supports many commands
 that can flip, brighten, or greyscale an image.
 
-The jar and script file are located in the /res folder. The jar is named assignment4.jar,
+The jar and script file are located in the /res folder. The jar is named Program.jar,
 and the script is name script.txt. To run the jar file, navigate to the res folder in
-command line and type "java -jar Assignment4.jar". To use the script file, add it as a command line
-argument: "java -jar Assignment4.jar script.txt"
+command line and type "java -jar Program.jar". To use the script file, add it as a command line
+argument: "java -jar Program.jar -file script.txt"
 
 NOTE: The script file is configured to run with the jar file. Due to relative paths, it
 will not work correctly if it is not used with the jar file.
@@ -161,7 +161,8 @@ class with "-file {path to script file}" as a command line argument.
 
 
 The .jar file has also been included in the /res folder. To use the jar file, simply
-navigate to the res folder in command line and type "java -jar Assignment4.jar"
+navigate to the res folder in command line and type "java -jar Program.jar" and append any 
+command line arguments.
 
 
 ### GUI Usage
--- src/imageprocessing/ImageProcessingProgram.java
+++ src/imageprocessing/ImageProcessingProgram.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Objects;
 import java.util.Scanner;
 
 import imageprocessing.controller.GraphicalController;
@@ -28,7 +27,6 @@ public class ImageProcessingProgram {
    * @param args Command line arguments.
    */
   public static void main(String[] args) {
-    //The default input is system.in unless a suitable script path was found and read.
     Readable input = new InputStreamReader(System.in);
     ImageProcessingModel model = new SimpleImageProcessingModel();
 
@@ -40,23 +38,29 @@ public class ImageProcessingProgram {
       ImageProcessingView view = new ImageProcessingViewImpl(model, System.out);
       ImageProcessingController controller = null;
 
-      if (Objects.equals(args[0], "-text")) {
+      if (args[0].equalsIgnoreCase("-text")) {
         controller =
                 new ImageProcessingControllerImpl(model, view, new InputStreamReader(System.in));
-      } else if (args[0].equals("-file") && args.length >= 2) {
+      } else if(args[0].equalsIgnoreCase("-file") && args.length == 1) {
+        System.out.println("No script file was entered, exiting now");
+        System.exit(0);
+      } else if (args[0].equalsIgnoreCase("-file") && args.length >= 2) {
         try {
           String scriptCommands = readScript(args[1]);
           input = new StringReader(scriptCommands);
         } catch (IllegalArgumentException e) {
-          System.out.println("Reading from script failed, readable set to System.in");
+          System.out.println("Failed to read from the script file, exiting now");
+          System.exit(0);
         }
         controller = new ImageProcessingControllerImpl(model, view, input);
 
+      } else {
+        System.out.println("Unrecognized command line argument, exiting now");
+        System.exit(0);
       }
 
-      if (controller != null) {
-        controller.start();
-      }
+      controller.start();
+
 
     }
 
